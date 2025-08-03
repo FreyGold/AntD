4dc537434103fb9b01b85025dcd09ac63385f772
freygold <ahmedtawfik833@gmail.com>
2025-08-04 01:57:16 +0300

    fix: remove unused vars

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,7 +3,7 @@ import LoginButton from "@/components/shared/LoginButton";
 function Auth() {
    return (
       <div className="flex w-screen h-screen justify-center items-center bg-[url('https://buguard.io/_next/static/media/dark-hero.2a187db0.png')] bg-cover">
-         <div className="card flex justify-center items-center w-[20%] h-[20%] backdrop-blur-2xl bg-background rounded-4xl flex-col gap-20 p-6 bg-red-400/20">
+         <div className="card flex justify-center items-center w-[20%] h-[20%] backdrop-blur-2xl rounded-4xl flex-col gap-20 p-6 bg-red-400/20">
             <h1 className="text-white">You must login to continue!</h1>
             <LoginButton className="w-full" />
          </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,3 @@
-import LoginButton from "@/components/shared/LoginButton";
-import LogoutButton from "@/components/shared/LogoutButton";
-import Profile from "@/components/shared/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Spin } from "antd";
 import { Outlet } from "react-router";
